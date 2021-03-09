5823b469f69bc91ba003954834f09610913da0b8
HuskyKingdom <1595025647@qq.com>
2021-03-09 18:48:02 +0800

    让杨总不别扭

--- tasks/views.py
+++ tasks/views.py
@@ -29,7 +29,7 @@ class delTask(APIView):
     permission_classes = (IsAuthenticated,)
     
     def post(self, request):
-        Task.objects.filter(user_id = request.user,task_id = request.data["task_id"]).update(is_delete = 1)
+        Task.objects.filter(user_id = request.user, task_id = request.data["task_id"]).update(is_delete = 1)
         return Response(
             data={"code": 200, "message": "Success!"},
             status=HTTP_200_OK
