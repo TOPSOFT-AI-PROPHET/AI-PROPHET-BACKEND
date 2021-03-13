cb93477c8380b66553a170795edea7a1c8e45caa
Steven <steven@stevens-Macbook.local>
2021-03-13 17:35:18 +0800

    用户修改密码

--- pay/migrations/0002_transaction_user_id.py
+++ pay/migrations/0002_transaction_user_id.py
@@ -11,12 +11,12 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('tasks', '0001_initial'),
+        ('pay', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='task',
+            model_name='transaction',
             name='user_id',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
