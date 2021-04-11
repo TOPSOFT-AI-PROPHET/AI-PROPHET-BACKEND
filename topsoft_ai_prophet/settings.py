57c2acbdf68ac3ade1b101f145bd6d1588781fb8
HuskyKingdom <1595025647@qq.com>
2021-04-11 19:17:54 +0800

    不要commit到这里！！！

--- topsoft_ai_prophet/settings.py
+++ topsoft_ai_prophet/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'aiprophet',
         'USER': 'root',
-        'PASSWORD': 'cp07031230',
+        'PASSWORD': 'asd69267822',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
