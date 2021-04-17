feb78d626b8d59dd006b9ab8cbc1d241f3f280e4
316423 <316423550@qq.com>
2021-04-17 22:10:19 +0800

    修改用户头像

--- users/views.py
+++ users/views.py
@@ -14,6 +14,7 @@ class updateUserProfile(APIView):
     permission_classes = (IsAuthenticated,)
     
     def post(self, request):
+        
         request.user.nickname = request.data["nickname"]
         request.user.contact_number = request.data["contact_number"]
         request.user.email = request.data["email"]
@@ -22,6 +23,41 @@ class updateUserProfile(APIView):
             data={"code": 200, "message": "Userinfo updated."},
             status=HTTP_200_OK
         )
+
+# 修改用户头像
+class updateUserProfileImage(APIView):
+    parser_classes = [MultiPartParser]
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+        file_obj = request.data['file']
+        
+        secret_id = 'AKIDZx60e1HAamulLgNW1MUR7WdT6UkktKp4'      # 替换为用户的 secretId
+        secret_key = '7xW4KOCiyyoN4WhbDySjjSu42kiPq1vx'      # 替换为用户的 secretKey
+        region = 'ap-chengdu'     # 替换为用户的 Region
+        token = None                # 使用临时密钥需要传入 Token，默认为空，可不填
+        scheme = 'https'            # 指定使用 http/https 协议来访问 COS，默认为 https，可不填
+        config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key, Token=token, Scheme=scheme)
+        # 2. 获取客户端对象
+        client = CosS3Client(config)
+        uuid_namespace = uuid.uuid3(uuid.NAMESPACE_OID,str(UserProfile.objects.get(id = request.user.id).id))
+        uuid_str = str(uuid.uuid3(uuid_namespace, "avatar"))
+        response = client.put_object(
+        Bucket='prophetsrc-1305001068',
+        Body=file_obj.read(),
+        Key= uuid_str,
+        StorageClass='STANDARD',
+        EnableMD5=False)
+        user = UserProfile.objects.get(id = request.user.id)
+        user.profile_image_uuid = uuid_str
+        user.save()
+
+        return Response(
+            data={"code": 200, "message": "Bingo!","ETag":response['ETag'],"uuid":uuid_str},
+            status=HTTP_200_OK
+        )
+
 
 # 获取用户信息
 class getUserInfo(APIView):
