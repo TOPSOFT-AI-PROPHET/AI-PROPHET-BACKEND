9ea961f7fede9ed45052e8a6d8a1996324378913
HuskyKingdom <1595025647@qq.com>
2021-04-27 17:33:57 +0800

    新增模型说明字段

--- tasks/models.py
+++ tasks/models.py
@@ -11,6 +11,7 @@ class AIModel(models.Model):
     ai_type = models.IntegerField(default=0)
     ai_credit = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     ai_introduction = models.TextField(verbose_name= 'ai introduction', null = True)
+    ai_true_description = models.CharField(max_length=100)
 
 class Task(models.Model):
     task_id = models.BigAutoField(primary_key=True) 
