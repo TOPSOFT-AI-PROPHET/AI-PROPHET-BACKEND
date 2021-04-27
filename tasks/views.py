0e44275c0c0dcb406c76cc29f0faaee810b013a5
HuskyKingdom <1595025647@qq.com>
2021-04-27 13:48:31 +0800

    修复了一个bug

--- tasks/views.py
+++ tasks/views.py
@@ -130,8 +130,8 @@ class numTask(APIView):
         permission_classes = (IsAuthenticated,)
 
         def post(self, request):
-            Task_num=Task.objects.filter(is_delete = 0).aggregate(Task_num = Count("task_id"))
-            Task_finish=Task.objects.filter(status = 100, is_delete = 0).aggregate(Task_finish = Count("task_id"))
+            Task_num=Task.objects.filter(is_delete = 0,user_id = request.user.id).aggregate(Task_num = Count("task_id"))
+            Task_finish=Task.objects.filter(status = 100, is_delete = 0 , user_id = request.user.id).aggregate(Task_finish = Count("task_id"))
 
             return Response(
             data={"code": 200, "data":{"num_of_task":str(Task_num['Task_num']),"num_of_finished_tasks":str(Task_finish['Task_finish'])}},
