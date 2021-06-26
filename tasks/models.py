e43bca5366f8d65fc72ba528af009c55fb1c5146
Steven <steven@stevens-Macbook.local>
2021-06-26 16:58:23 +0800

    update tasks/models.py

--- tasks/models.py
+++ tasks/models.py
@@ -8,9 +8,11 @@ class AIModel(models.Model):
     ai_url = models.TextField(verbose_name='ai link url')
     ai_status = models.IntegerField(default=0)
     ai_description = models.TextField(verbose_name='ai description')
+    # descibe how many parameters
     ai_type = models.IntegerField(default=0)
     ai_credit = models.DecimalField(max_digits=20, decimal_places=2, default=0)
-    ai_introduction = models.TextField(verbose_name= 'ai introduction', null = True)    
+    ai_true_description = models.TextField(verbose_name= 'ai introduction', null = True)
+    # general description about this model    
     ai_author = models.CharField(max_length=100, null = True)
     ai_published = models.BooleanField(default=False)
     ai_model_profile = models.TextField(verbose_name= 'ai model profile', null = True)
