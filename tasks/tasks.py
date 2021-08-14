69657d4277b9fbe70f9f04271fb2561ebfb7acb7
Matt <mwpryer@gmail.com>
2021-08-14 23:27:43 +0100

    Refactor cos utils

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,4 +1,3 @@
-
 from __future__ import absolute_import, unicode_literals
 from celery.task import task
 
@@ -6,7 +5,7 @@ from AI.newML import Data_split
 from AI.newML import my_Cross_Validation
 from AI.newML import Machine_Learning
 
-from utils.cos import write_model,read_model
+from common.utils.cos import write_model, read_model
 import uuid
 from .models import Task, AIModel
 from .models import UserProfile
@@ -15,23 +14,21 @@ import base64
 from io import BytesIO
 
 
-
-
 @task
-def ML_Traditional(file_str,uuid_str,ai_id):
-    
+def ML_Traditional(file_str, uuid_str, ai_id):
+
     dataset_file = BytesIO(base64.b64decode(file_str))
 
     model = Machine_Learning(dataset_file, 0.2)
 
-    write_model(uuid_str,model)
+    write_model(uuid_str, model)
     ai_instance = AIModel.objects.get(ai_id=ai_id)
     ai_instance.ai_status = 100
     ai_instance.save()
-    
+
 
 @task
 def test():
     time.sleep(10)
     print("finished!")
-    return 'hello'
+    return "hello"
